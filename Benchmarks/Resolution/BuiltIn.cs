6cb920657edc8a417f72bd5691845f194247e28b
Eugene Sadovoi <evgeni@eniks.com>
2023-05-26 09:32:42 -0700

    More benchmarks

--- Benchmarks/Resolution/BuiltIn.cs
+++ Benchmarks/Resolution/BuiltIn.cs
@@ -4,12 +4,6 @@ namespace Unity.Benchmarks
 {
     public partial class ResolutionBenchmarks
     {
-        [Benchmark(Description = "Resolve<object>()")]
-        [BenchmarkCategory("resolve", "IUnityContainer")]
-        public object ResolveObject()
-            => Container.Resolve(typeof(object));
-
-
         [BenchmarkCategory("resolve", "IUnityContainer")]
         [Benchmark(Description = "Resolve<IUnityContainer>( )")]
         public object ResolveIUnityContainer()
